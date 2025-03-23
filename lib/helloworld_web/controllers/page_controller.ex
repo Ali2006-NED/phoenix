5b78d99b9e414c5db7120313e91988f943df639c
Ali2006-NED <aliahmed23022006@gmail.com>
2025-03-24 02:28:30 +0500

    Refactor: Update login page and require authentication for home page
    This commit refactors the login page to use simple_form, adds password visibility toggle, and enforces authentication for the home page by redirecting unauthenticated users to the login page. The header and register links were also removed.

--- lib/helloworld_web/controllers/page_controller.ex
+++ lib/helloworld_web/controllers/page_controller.ex
@@ -1,9 +1,19 @@
 defmodule HelloworldWeb.PageController do
   use HelloworldWeb, :controller
 
+  plug :redirect_if_not_logged_in when action in [:home]
+
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, "home.html")
+  end
+
+  defp redirect_if_not_logged_in(conn, _params) do
+    if conn.assigns[:current_user] do
+      conn  # Allow access if user is logged in
+    else
+      conn
+      |> redirect(to: "/users/log_in")  # Redirect to login if not logged in
+      |> halt()
+    end
   end
 end
